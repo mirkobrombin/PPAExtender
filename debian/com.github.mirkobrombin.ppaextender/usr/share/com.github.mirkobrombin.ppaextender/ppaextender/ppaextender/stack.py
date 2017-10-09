6477c9588575edc5217eb8647f993b6dc1906a6d
Mirko Brombin <brombinmirko@gmail.com>
2017-10-09 11:41:30 +0200

    Stuff

---
+++ debian/com.github.mirkobrombin.ppaextender/usr/share/com.github.mirkobrombin.ppaextender/ppaextender/ppaextender/stack.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''
+   Copyright 2017 Mirko Brombin (brombinmirko@gmail.com)
+
+   This file is part of PPAExtender.
+
+    PPAExtender is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PPAExtender is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PPAExtender.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+import gi
+import webbrowser
+gi.require_version('Gtk', '3.0')
+gi.require_version('Granite', '1.0')
+from gi.repository import Gtk, Gdk, Granite
+try:
+    import constants as cn
+    import welcome as wl
+    import detail as dt
+    import list as ls
+except ImportError:
+    import ppaextender.constants as cn
+    import ppaextender.welcome as wl
+    import ppaextender.detail as dt
+    import ppaextender.list as ls
+
+class Stack(Gtk.Box):
+        
+    # Define variable for GTK global theme
+    settings = Gtk.Settings.get_default()
+
+    def __init__(self, parent):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.parent = parent
+
+        self.stack = Gtk.Stack()
+        self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        self.stack.set_transition_duration(1000)
+        
+        self.welcome = wl.Welcome(self)
+        self.detail = dt.Detail(self)
+        self.list_all = ls.List(self)
+
+        self.stack.add_titled(self.welcome, "welcome", "Welcome")
+        self.stack.add_titled(self.detail, "detail", "Detail")
+        self.stack.add_titled(self.list_all, "list", "List")
+
+        self.pack_start(self.stack, True, True, 0)
