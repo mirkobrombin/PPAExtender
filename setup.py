7c03d914b50c9f3edbab203808b41ec2966e6196
Ian Santopietro <ian@system76.com>
2018-01-03 15:27:51 -0700

    Merge jderose/disable-ppa

--- setup.py
+++ setup.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
 
-import glob, os
 from distutils.core import setup
 
-install_data = [('share/metainfo', ['data/xyz.isantop.repoman.appdata.xml']),
-                ('share/applications', ['data/xyz.isantop.repoman.desktop']),
-                ('bin/repoman',['data/style.css']),
-                ('bin/repoman',['repoman/constants.py']),
-                ('bin/repoman',['repoman/dialog.py']),
-                ('bin/repoman',['repoman/headerbar.py']),
-                ('bin/repoman',['repoman/list.py']),
-                ('bin/repoman',['repoman/main.py']),
-                ('bin/repoman',['repoman/ppa.py']),
-                ('bin/repoman',['repoman/settings.py']),
-                ('bin/repoman',['repoman/stack.py']),
-                ('bin/repoman',['repoman/updates.py']),
-                ('bin/repoman',['repoman/window.py']),
-                ('bin/repoman',['repoman/__init__.py']),
-                ('bin/repoman',['pkexec'])]
-
-setup(  name='Repoman',
-        version='0.0.6',
-        author='Ian Santopietro',
-        description='Easily manage PPA',
-        url='https://github.com/isantop/repoman',
-        license='GNU GPL3',
-        scripts=['xyz.isantop.repoman'],
-        packages=['repoman'],
-        data_files=install_data)
+setup(
+    name='Repoman',
+    version='0.0.6',
+    author='Ian Santopietro',
+    description='Easily manage PPAs',
+    url='https://github.com/isantop/repoman',
+    license='GNU GPL3',
+    scripts=['xyz.isantop.repoman'],
+    packages=['repoman'],
+    data_files=[
+        ('share/metainfo', ['data/xyz.isantop.repoman.appdata.xml']),
+        ('share/applications', ['data/xyz.isantop.repoman.desktop']),
+        ('share/repoman', ['data/style.css']),
+        ('lib/repoman', ['pkexec']),
+    ],
+)
