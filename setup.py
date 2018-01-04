974be8278ba1cad011941f6a5e5439e3c63e5165
Ian Santopietro <ian@system76.com>
2018-01-03 18:43:51 -0700

    Recreate setup.py, add  .gitignore
    This makes the file much more pythonic and won't require tracking
    adding or removing files in setup.py.
    .gitignore is set up to ignore build artefacts.

--- setup.py
+++ setup.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
 
-import glob, os
 from distutils.core import setup
 
-install_data = [('share/metainfo', ['data/com.github.mirkobrombin.ppaextender.appdata.xml']),
+setup(
+    name='PPA Extender',
+    version='0.0.1',
+    author='Mirko Brombin',
+    description='Easily manage PPA',
+    url='https://github.com/mirkobrombin/ppaextender',
+    license='GNU GPL3',
+    scripts=['com.github.mirkobrombin.ppaextender'],
+    packages=['ppaextender'],
+    data_files=[('share/metainfo', ['data/com.github.mirkobrombin.ppaextender.appdata.xml']),
                 ('share/applications', ['data/com.github.mirkobrombin.ppaextender.desktop']),
                 ('share/icons/hicolor/128x128/apps',['data/com.github.mirkobrombin.ppaextender.svg']),
-                ('bin/ppaextender',['data/style.css']),
-                ('bin/ppaextender',['ppaextender/constants.py']),
-                ('bin/ppaextender',['ppaextender/detail.py']),
-                ('bin/ppaextender',['ppaextender/headerbar.py']),
-                ('bin/ppaextender',['ppaextender/helper.py']),
-                ('bin/ppaextender',['ppaextender/list.py']),
-                ('bin/ppaextender',['ppaextender/main.py']),
-                ('bin/ppaextender',['ppaextender/ppa.py']),
-                ('bin/ppaextender',['ppaextender/stack.py']),
-                ('bin/ppaextender',['ppaextender/welcome.py']),
-                ('bin/ppaextender',['ppaextender/window.py']),
-                ('bin/ppaextender',['ppaextender/__init__.py']),
-                ('bin/ppaextender',['pkexec'])]
-
-setup(  name='PPA Extender',
-        version='0.0.1',
-        author='Mirko Brombin',
-        description='Easily manage PPA',
-        url='https://github.com/mirkobrombin/ppaextender',
-        license='GNU GPL3',
-        scripts=['com.github.mirkobrombin.ppaextender'],
-        packages=['ppaextender'],
-        data_files=install_data)
+                ('share/ppaextender',['data/style.css']),
+                ('lib/ppaextender',['pkexec']),
+    ],
+)
