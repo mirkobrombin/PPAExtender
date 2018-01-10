86bd06a1908cc3c2cf016e2e8e21373a8a968e79
Ian Santopietro <ian@system76.com>
2018-01-10 16:29:37 -0700

    Change setup.py and structure to a standard working format.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='Easily manage PPAs',
     url='https://github.com/isantop/repoman',
     license='GNU GPL3',
-    scripts=['xyz.isantop.repoman'],
+    scripts=['repoman/repoman'],
     packages=['repoman'],
     data_files=[
         ('share/metainfo', ['data/xyz.isantop.repoman.appdata.xml']),
@@ -23,6 +23,6 @@ setup(
         ('share/repoman', ['data/style.css']),
         ('share/repoman/po/es/LC_MESSAGES', ['po/es/repoman.mo']),
         ('share/repoman/po/sv/LC_MESSAGES', ['po/sv/repoman.mo']),
-        ('lib/repoman', ['xyz.isantop.repoman.pkexec']),
+        ('lib/repoman', ['repoman.pkexec']),
     ],
 )
