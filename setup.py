8425a0ddef1ab88cf9b8b20d86321c49ce30b22d
Ian Santopietro <isantop@gmail.com>
2018-08-10 10:20:33 -0600

    Upstream master (#16)
    * Fix up the dialog
    
    * Add colons and increase row spacing
    
    * Revert column spacing decrease
    
    * Better PolKit-1 dialogs
    
    * Policy changes for better PolicyKit use
    
    * Ignore .buildconfig file
    
    * Remove inline-toolbar-styling
    
    * Release version 0.0.7
    
    * remove debhelper files
    
    * Remove entry for unused desktop file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Repoman',
-    version='1.0.0',
+    version='1.0.1',
     author='Ian Santopietro',
     description='Easily manage PPAs',
     url='https://github.com/isantop/repoman',
