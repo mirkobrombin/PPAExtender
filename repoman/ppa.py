cd82f4cd28c21e4ed1d456d44ab5cb0342191702
Ian Santopietro <ian@system76.com>
2019-11-19 15:11:01 -0700

    Fix incorrect name

--- repoman/ppa.py
+++ repoman/ppa.py
@@ -108,6 +108,7 @@ class ModifyThread(threading.Thread):
     def __init__(self, parent, old_source, new_source, sp):
         threading.Thread.__init__(self)
         self.parent = parent
+        print(old_source)
         self.old_source = old_source
         self.new_source = new_source
         self.sp = sp
@@ -121,7 +122,7 @@ class ModifyThread(threading.Thread):
 
     def run(self):
         try:
-            privileged_object.modify_repo(old_source, new_source)
+            privileged_object.modify_repo(self.old_source, self.new_source)
             self.sp.reload_sourceslist()
         except:
             self.exc = sys.exc_info()
