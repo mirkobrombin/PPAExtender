49353ced3d63b6ff46e0b300064960fe51475b6c
Ian Santopietro <ian@system76.com>
2019-11-19 11:33:06 -0700

    Add classmethods

--- data/service.py
+++ data/service.py
@@ -62,4 +62,77 @@ class PPA(dbus.service.Object):
         self._check_polkit_privilege(
             sender, conn, 'org.pop-os.repoman.modifysources'
         )
+    
+    @dbus.service.method(
+        "ro.santopiet.repoman.Interface",
+        in_signature='', out_signature='',
+        sender_keyword='sender', connection_keyword='conn'
+    )
+    def Exit(self, sender=None, conn=None):
+        mainloop.quit()
+
+    @classmethod
+    def _log_in_file(klass, filename, string):
+        date = time.asctime(time.localtime())
+        ff = open(filename, "a")
+        ff.write("%s : %s\n" %(date,str(string)))
+        ff.close()
+    
+    @classmethod
+    def _strip_source_line(self, source):
+        source = source.replace("#", "# ")
+        source = source.replace("[", "")
+        source = source.replace("]", "")
+        source = source.replace("'", "")
+        source = source.replace("  ", " ")
+        return source
+
+    def _check_polkit_privilege(self, sender, conn, privilege):
+        # from jockey
+        '''Verify that sender has a given PolicyKit privilege.
+        sender is the sender's (private) D-BUS name, such as ":1:42"
+        (sender_keyword in @dbus.service.methods). conn is
+        the dbus.Connection object (connection_keyword in
+        @dbus.service.methods). privilege is the PolicyKit privilege string.
+        This method returns if the caller is privileged, and otherwise throws a
+        PermissionDeniedByPolicy exception.
+        '''
+        if sender is None and conn is None:
+            # called locally, not through D-BUS
+            return
+        if not self.enforce_polkit:
+            # that happens for testing purposes when running on the session
+            # bus, and it does not make sense to restrict operations here
+            return
+
+        # get peer PID
+        if self.dbus_info is None:
+            self.dbus_info = dbus.Interface(conn.get_object('org.freedesktop.DBus',
+                '/org/freedesktop/DBus/Bus', False), 'org.freedesktop.DBus')
+        pid = self.dbus_info.GetConnectionUnixProcessID(sender)
         
+        # query PolicyKit
+        if self.polkit is None:
+            self.polkit = dbus.Interface(dbus.SystemBus().get_object(
+                'org.freedesktop.PolicyKit1',
+                '/org/freedesktop/PolicyKit1/Authority', False),
+                'org.freedesktop.PolicyKit1.Authority')
+        try:
+            # we don't need is_challenge return here, since we call with AllowUserInteraction
+            (is_auth, _, details) = self.polkit.CheckAuthorization(
+                    ('unix-process', {'pid': dbus.UInt32(pid, variant_level=1),
+                    'start-time': dbus.UInt64(0, variant_level=1)}), 
+                    privilege, {'': ''}, dbus.UInt32(1), '', timeout=600)
+        except dbus.DBusException as e:
+            if e._dbus_error_name == 'org.freedesktop.DBus.Error.ServiceUnknown':
+                # polkitd timed out, connect again
+                self.polkit = None
+                return self._check_polkit_privilege(sender, conn, privilege)
+            else:
+                raise
+
+        if not is_auth:
+            PPA._log_in_file('/tmp/repoman.log','_check_polkit_privilege: sender %s on connection %s pid %i is not authorized for %s: %s' %
+                    (sender, conn, pid, privilege, str(details)))
+            raise PermissionDeniedByPolicy(privilege)
+
