060aed43b56a543c5e986481855d435492d57588
Ian Santopietro <ian@system76.com>
2020-11-06 13:17:19 -0700

    fix(repo): escape markup text in secondary text of error dialogs
    We're setting markup for the error message. However, if the exception contains markup characters, it can cause markup parsing errors that prevent the text from being set. Using Glib.markup_escape_text() ensures that any output we get from the exception will be properly escaped before we set it.

--- repoman/repo.py
+++ repoman/repo.py
@@ -193,7 +193,8 @@ def get_error_messagedialog(parent, text, exc, prefix):
     )
 
     traceback_text = ' '.join(traceback.format_tb(exc.__traceback__))
-    dialog.format_secondary_markup(f'{prefix}:\n{str(exc)}')
+    secondary_text = GLib.markup_escape_text(str(exc))
+    dialog.format_secondary_markup(f'{prefix}:\n{secondary_text}')
     content_area = dialog.get_content_area()
     
     expander = Gtk.Expander.new('Error details:')
